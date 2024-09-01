032c446fd32de9652c153b9324d9ce7c0ef4f1ce
emmalunar <eoppong416gmail.com>
2024-09-02 00:25:52 +0100

    summing the range of numbers in n

--- index.py
+++ index.py
@@ -2,4 +2,23 @@ def sum_of_numbers(x, y):
     return x + y
 
 z = sum_of_numbers(2, 3)
+# print(z)
+
+def add_to_n(n):
+
+    sum = 0
+
+    for i in range(1, n+1):
+        sum += i
+    return sum
+
+
+
+z = add_to_n(19)
 print(z)
+    
+
+
+# if n is 3, the function will return 1+2+3 = 6.
+
+
